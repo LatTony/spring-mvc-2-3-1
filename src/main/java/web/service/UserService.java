1b4015bc29c275ae69ac780a4725be333816983f
TonyLat <antohahaha91@gmail.com>
2022-11-12 15:33:15 +0300

    2.3.1

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,16 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers();
+
+    void save(User user);
+
+    User getUser(int id);
+
+    void delete(int id);
+}
